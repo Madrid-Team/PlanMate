8262a97804bd3ba3387600b1182d18510479df6a
khater <khater@email.com>
2025-04-28 13:41:19 +0300

    add project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "org.madrid"
@@ -10,7 +11,23 @@ repositories {
 }
 
 dependencies {
+    // TEST
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // KOIN
+    implementation("io.insert-koin:koin-core:3.5.3")
+    // JUPITER
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    // TRUTH
+    testImplementation ("com.google.truth:truth:1.4.4")
+    // MOCKK
+    testImplementation("io.mockk:mockk:1.14.0")
+    // JSON
+    implementation("com.google.code.gson:gson:2.10.1")
+    // SERIALIZATION
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
 }
 
 tasks.test {
