4486f46a778de846b5b1b6e54c1baa9857fe792d
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-29 16:35:59 +0300

    create UserRepository interface

---
+++ src/main/kotlin/domain/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package domain.repository
+
+import domain.models.authentication.User
+
+interface UserRepository {
+    fun deleteUser(userId: String)
+    fun addUser(user: User)
+    fun getUser(userId: String): User?
+}
