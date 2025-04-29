ca822fba64093020c4a6a55ef3a19da45dcdf320
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-30 02:54:07 +0300

    add InvalidInput exception to UserException

--- src/main/kotlin/domain/utlis/UserException.kt
+++ src/main/kotlin/domain/utlis/UserException.kt
@@ -2,6 +2,6 @@ package domain.utlis
 
 sealed class UserException(message: String) : RuntimeException(message) {
     class NotFoundUser(message: String) : UserException(message)
-    class InvalidPassword(message: String) : UserException(message)
+    class InvalidInput(message: String) : UserException(message)
     class PermissionDenied(message: String) : UserException(message)
 }
