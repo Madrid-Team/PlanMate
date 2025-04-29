b3d6fde019fd3d000f9709e6fcc025bce143db9e
islam elhady <i.elhady44@gmail.com>
2025-04-29 09:19:27 +0300

    test: add tests for hint printing behavior when hint is empty and not empty

--- src/test/kotlin/presentation/components/ConsoleInputReaderTest.kt
+++ src/test/kotlin/presentation/components/ConsoleInputReaderTest.kt
@@ -3,6 +3,8 @@ package presentation.components
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.BeforeEach
 import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 import kotlin.test.Test
 
 class ConsoleInputReaderTest {
@@ -43,4 +45,37 @@ class ConsoleInputReaderTest {
         assertThat(result).isEqualTo("")
     }
 
+    @Test
+    fun `should print hint when hint is not empty`() {
+        // given
+        val hint = "enter your name"
+        val input = "islam elhady"
+        val inputStream = ByteArrayInputStream(input.toByteArray())
+        System.setIn(inputStream)
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        // when
+        consoleInputReader.readInput(hint)
+
+        // then
+        assertThat(outputStream.toString()).contains(hint)
+    }
+
+    @Test
+    fun `should not print anything when hint is empty`() {
+        // given
+        val hint = ""
+        val input = "islam elhady"
+        val inputStream = ByteArrayInputStream(input.toByteArray())
+        System.setIn(inputStream)
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        // when
+        consoleInputReader.readInput(hint)
+
+        // then
+        assertThat(outputStream.toString()).isEmpty()
+    }
 }
