7db1c3c4230fc09b03ae25be3019adcf39f4b10b
islam elhady <i.elhady44@gmail.com>
2025-04-29 10:00:15 +0300

    implement ConsoleOutputPrinter to pass tests

--- src/main/kotlin/presentation/components/ConsoleOutputPrinter.kt
+++ src/main/kotlin/presentation/components/ConsoleOutputPrinter.kt
@@ -2,10 +2,10 @@ package presentation.components
 
 class ConsoleOutputPrinter : OutputPrinter {
     override fun printMessage(message: String) {
-        TODO("Not yet implemented")
+        println(message)
     }
 
     override fun printError(errorMessage: String) {
-        TODO("Not yet implemented")
+        println(errorMessage)
     }
 }
