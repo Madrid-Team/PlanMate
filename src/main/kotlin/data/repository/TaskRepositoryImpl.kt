f19981ec60221bf3c1e8922bc576d6e7ff567a04
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-29 17:27:20 +0300

    add task structure in data layer

---
+++ src/main/kotlin/data/repository/TaskRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package data.repository
+
+import domain.repository.TaskRepository
+
+class TaskRepositoryImpl : TaskRepository {
+}
---
+++ src/main/kotlin/data/source/TaskDataSource.kt
@@ -0,0 +1,4 @@
+package data.source
+
+interface TaskDataSource {
+}
