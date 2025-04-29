cdc01093d2b2e33f367c0500285e2eb39406e4ab
islam elhady <i.elhady44@gmail.com>
2025-04-29 08:30:44 +0300

    add test for readInput to ensure input is returned correctly

---
+++ src/test/kotlin/presentation/components/ConsoleInputReaderTest.kt
@@ -0,0 +1,35 @@
+package presentation.components
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.BeforeEach
+import java.io.ByteArrayInputStream
+import kotlin.test.Test
+
+class ConsoleInputReaderTest {
+
+    private lateinit var consoleInputReader: ConsoleInputReader
+
+    @BeforeEach
+    fun setup() {
+        consoleInputReader = ConsoleInputReader()
+    }
+
+
+    @Test
+    fun `should return input when readInput is called`() {
+        // given
+        val input = "islam elhady"
+        val inputStream = ByteArrayInputStream(input.toByteArray())
+        System.setIn(inputStream)
+
+        // when
+        val result = consoleInputReader.readInput("enter your name")
+
+        // then
+        assertThat(result).isEqualTo(input)
+    }
+
+
+
+
+}
