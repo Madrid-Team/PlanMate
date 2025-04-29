2bf2a3d18bf809562172c5759b4311b290550324
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-29 16:37:59 +0300

    create FakeUser to use in tests

---
+++ src/test/kotlin/FakeUser.kt
@@ -0,0 +1,19 @@
+import domain.models.authentication.User
+import domain.repository.UserRepository
+
+class FakeUser : UserRepository {
+
+    val allUsers = mutableMapOf<String, User>()
+
+    override fun deleteUser(userId: String) {
+        allUsers.remove(userId)
+    }
+
+    override fun addUser(user: User) {
+        allUsers[user.id] = user
+    }
+
+    override fun getUser(userId: String): User? {
+        return allUsers[userId]
+    }
+}
