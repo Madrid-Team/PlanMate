fee2dab87613c791ba49885c24c32c2c6c28085a
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-30 16:15:55 +0300

    Add unit test setup for DisplayAllTasksUseCase

--- src/test/kotlin/domain/usecases/DisplayAllTasksUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/DisplayAllTasksUseCaseTest.kt
@@ -1,7 +1,21 @@
 package domain.usecases
 
-import org.junit.jupiter.api.Assertions.*
- class DisplayAllTasksUseCaseTest{
+import domain.repository.ProjectRepository
+import domain.repository.TaskRepository
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+
+class DisplayAllTasksUseCaseTest {
+    private lateinit var projectRepository: ProjectRepository
+    private lateinit var taskRepository: TaskRepository
+    private lateinit var displayAllTasksUseCase: DisplayAllTasksUseCase
+
+    @BeforeEach
+    fun setUp() {
+        projectRepository = mockk()
+        taskRepository = mockk()
+        displayAllTasksUseCase = DisplayAllTasksUseCase(projectRepository, taskRepository)
+    }
 
 
- }
+}
