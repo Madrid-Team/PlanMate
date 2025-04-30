7f24b8b1f6c968a577d8c0673920ff22bb0d4de4
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-30 20:06:22 +0300

    implement UserCsvParser class

--- src/main/kotlin/data/source/user/UserCsvParser.kt
+++ src/main/kotlin/data/source/user/UserCsvParser.kt
@@ -2,17 +2,30 @@ package data.source.user
 
 import data.dto.authentication.User
 import data.dto.authentication.UserRole
+import data.utils.PlanMateColumnIndex.UserColumnsIndex
 
 
 class UserCsvParser {
+    val userColumnsIndex = UserColumnsIndex
 
     fun parseRowToUser(row: String): User {
-        // TODO parse row to User data class
-        return User("", "", "", UserRole.MATE)
+        val result = row.split(",")
+        return User(
+            id = result[userColumnsIndex.USER_ID],
+            username = result[userColumnsIndex.USER_NAME],
+            passwordHash = result[userColumnsIndex.USER_PASSWORD_HASH],
+            role = UserRole.valueOf(result[userColumnsIndex.USER_ROLE]),
+        )
     }
 
     fun parseUserToRow(user: User): String {
-        // TODO parse User data class to row
-        return ""
+        val userCsvLine = listOf(
+            user.id,
+            user.username,
+            user.passwordHash,
+            user.role.name,
+        ).joinToString(",")
+
+        return userCsvLine
     }
 }
