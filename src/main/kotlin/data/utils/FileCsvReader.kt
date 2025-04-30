4c0cda07f20f1a359b1fdc25eae2cec71d52f2b2
khater <khater@email.com>
2025-04-30 18:59:40 +0300

    implement file reader and write and parser classes

--- src/main/kotlin/data/utils/FileCsvReader.kt
+++ src/main/kotlin/data/utils/FileCsvReader.kt
@@ -1,10 +1,17 @@
 package data.utils
 
+import java.io.FileNotFoundException
+import java.io.IOException
+
 class FileCsvReader(
     private val fileValidator: FileValidator
 ) {
     fun readProjectCsvFile(): List<String> {
-        return fileValidator.getFile().readLines().dropHeader()
+        return try {
+            fileValidator.getFile().readLines().dropHeader()
+        } catch (e: FileNotFoundException){
+            throw e
+        }
     }
 
     private fun List<String>.dropHeader(): List<String> = this.drop(1)
