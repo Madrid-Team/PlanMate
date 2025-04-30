b8b0fb345bb36f4210d1919fa414077c65195208
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-30 16:43:15 +0300

    Add test for handling non-existing project in DisplayAllTasksUseCase

--- src/test/kotlin/domain/usecases/DisplayAllTasksUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/DisplayAllTasksUseCaseTest.kt
@@ -1,9 +1,12 @@
 package domain.usecases
 
+import com.google.common.truth.Truth.assertThat
 import domain.repository.ProjectRepository
 import domain.repository.TaskRepository
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class DisplayAllTasksUseCaseTest {
     private lateinit var projectRepository: ProjectRepository
@@ -17,5 +20,17 @@ class DisplayAllTasksUseCaseTest {
         displayAllTasksUseCase = DisplayAllTasksUseCase(projectRepository, taskRepository)
     }
 
+    @Test
+    fun `should return Project not found message when project does not exist`() {
+        // Given
+        val projectId = "non_existing_project"
+        every { projectRepository.getProjectById(projectId) } returns null
 
-}
+        // When
+        val result = displayAllTasksUseCase.display(projectId)
+
+        // Then
+        assertThat(result).isEqualTo("Project not found.")
+    }
+
+    }
