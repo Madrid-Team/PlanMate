39f699a35f176b84602cd43b32c746cb9bee1d9d
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-30 16:21:03 +0300

    add display function to DisplayAllTasksUseCase class

--- src/main/kotlin/domain/usecases/DisplayAllTasksUseCase.kt
+++ src/main/kotlin/domain/usecases/DisplayAllTasksUseCase.kt
@@ -3,8 +3,11 @@ package domain.usecases
 import domain.repository.ProjectRepository
 import domain.repository.TaskRepository
 
-class DisplayAllTasksUseCase(private val projectRepository: ProjectRepository,
-                             private val taskRepository: TaskRepository
-)
-{
+class DisplayAllTasksUseCase(
+    private val projectRepository: ProjectRepository,
+    private val taskRepository: TaskRepository
+) {
+    fun display(projectId: String): String {
+        return "Project not found."
+    }
 }
