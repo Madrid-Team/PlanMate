e7b97b5badb77a43ab23e8459d8a60c615829c01
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-30 19:55:18 +0300

    add delete test case logic

--- src/main/kotlin/domain/usecases/DeleteTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/DeleteTaskUseCase.kt
@@ -1,11 +1,19 @@
 package domain.usecases
 
+import domain.models.task.Task
 import domain.repository.TaskRepository
+import domain.utlis.TaskNotFoundException
 
 class DeleteTaskUseCase(
     private val taskRepository: TaskRepository
 ) {
     fun deleteTask(taskId: String): Boolean {
-        return false
+        getTaskById(taskId) ?: throw TaskNotFoundException()
+        taskRepository.deleteTask(taskId)
+        return true
+    }
+
+    private fun getTaskById(taskId: String): Task? {
+        return taskRepository.getAllTasks().find { it.id == taskId }
     }
 }
