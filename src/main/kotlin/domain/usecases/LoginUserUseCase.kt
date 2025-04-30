1407ac5a676d137508206f5cd49cd0f3d099c328
Abdelrahmanibrrahem <ai4516299@gmail.com>
2025-04-30 20:10:46 +0300

    add Hashing to LoginUserUseCase implementation

--- src/main/kotlin/domain/usecases/LoginUserUseCase.kt
+++ src/main/kotlin/domain/usecases/LoginUserUseCase.kt
@@ -1,12 +1,13 @@
 package domain.usecases
 
 
+import data.utils.PasswordHasher
 import domain.models.authentication.User
 import domain.repository.UserRepository
 import domain.utlis.UserException
 
 
-class LoginUserUseCase ( private val userRepository: UserRepository){
+class LoginUserUseCase ( private val userRepository: UserRepository ,private val passwordHasher: PasswordHasher   ){
 
     fun invoke(userName: String,password: String): User{
 
@@ -15,7 +16,7 @@ class LoginUserUseCase ( private val userRepository: UserRepository){
     private fun validateUser(users: List<User>, username: String, password: String): User{
         val user=users.find { it.username==username }
             ?: throw UserException.NotFoundUser("Not found user or wrong username")
-        if(user.passwordHash!=password){
+        if(user.passwordHash!=passwordHasher.hash(password)){
             throw UserException.WrongPasswordOrUserName("Wrong  password")
         }
         return user
