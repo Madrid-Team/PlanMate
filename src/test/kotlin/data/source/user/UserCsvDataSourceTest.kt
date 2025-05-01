40bfe0c7aacb286b76c1eda9398e493928f97ac4
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-01 15:05:42 +0300

    add fake user to UserCsvDataSourceTest

--- src/test/kotlin/data/source/user/UserCsvDataSourceTest.kt
+++ src/test/kotlin/data/source/user/UserCsvDataSourceTest.kt
@@ -1,5 +1,7 @@
 package data.source.user
 
+import data.dto.authentication.UserDto
+import data.dto.authentication.UserRoleDto
 import data.utils.FileCsvReader
 import data.utils.FileCsvWriter
 import io.mockk.mockk
@@ -12,6 +14,21 @@ class UserCsvDataSourceTest {
     private lateinit var userCsvParser: UserCsvParser
     private lateinit var dataSource: UserCsvDataSource
 
+    private val user1 = UserDto(
+        id = "1",
+        username = "username1",
+        passwordHash = "passwordhash1",
+        role = UserRoleDto.ADMIN,
+    )
+    private val user2 = UserDto(
+        id = "2",
+        username = "username2",
+        passwordHash = "passwordhash2",
+        role = UserRoleDto.MATE,
+    )
+    private val row1 = "1,username1,passwordhash1,ADMIN"
+    private val row2 = "2,username2,passwordhash2,MATE"
+
     @BeforeEach
     fun setUp() {
         fileCsvWriter = mockk()
@@ -19,6 +36,5 @@ class UserCsvDataSourceTest {
         userCsvParser = mockk()
         dataSource = UserCsvDataSource(fileCsvReader, fileCsvWriter, userCsvParser)
     }
-
 
 }
