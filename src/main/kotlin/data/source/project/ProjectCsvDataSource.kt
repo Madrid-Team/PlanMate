fc6e93346c7cbbeb700fd2ee424cf367c5154bf5
khater <khater@email.com>
2025-05-01 18:13:00 +0300

    implement delete project function in ProjectCsvDataSource.kt

--- src/main/kotlin/data/source/project/ProjectCsvDataSource.kt
+++ src/main/kotlin/data/source/project/ProjectCsvDataSource.kt
@@ -1,15 +1,15 @@
 package data.source.project
 
+import data.dto.project.ProjectDto
 import data.utils.FileCsvReader
 import data.utils.FileCsvWriter
-import data.dto.project.ProjectDto
 
 class ProjectCsvDataSource(
     private val fileCsvReader: FileCsvReader,
     private val fileCsvWriter: FileCsvWriter,
     private val projectCsvParser: ProjectCsvParser
 ) : ProjectDataSource {
-    override fun getAllProjects(): List<ProjectDto> {
+    override fun getProjects(): Result<List<ProjectDto>> {
         TODO("Not yet implemented")
     }
 
@@ -17,8 +17,18 @@ class ProjectCsvDataSource(
         TODO()
     }
 
-    override fun deleteProject(projectId: String): Result<Unit> {
-        TODO()
+    override fun deleteProject(projects: List<ProjectDto>): Result<Unit> {
+        return try {
+            var projectFileContentAfterDeletion = ""
+            projects.forEach {
+                val projectAsString = projectCsvParser.parseProjectToString(it) + "\n"
+                projectFileContentAfterDeletion += projectAsString
+             }
+            fileCsvWriter.updateCsvFile(projectFileContentAfterDeletion)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
     override fun editProject(project: ProjectDto): Result<Unit> {
