53dfa5e03d9bbbbe7ad86d990a5cc32fa14062b2
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-01 08:33:00 +0300

    DisplayAllTasksUseCase

--- src/main/kotlin/domain/usecases/DisplayAllTasksUseCase.kt
+++ src/main/kotlin/domain/usecases/DisplayAllTasksUseCase.kt
@@ -7,37 +7,4 @@ class DisplayAllTasksUseCase(
     private val projectRepository: ProjectRepository,
     private val taskRepository: TaskRepository
 ) {
-    fun display(projectId: String): String {
-        val project = projectRepository.getProjectById(projectId)
-            ?: return "Project not found."
-        val tasks = taskRepository.getTasksByProjectId(projectId)
-        val states = project.taskStates
-        val swimlanesMap = states.associateWith { mutableListOf<String>() }
-
-        for (task in tasks) {
-            swimlanesMap[task.state]?.add(task.title)
-        }
-
-        return formatAsSwimlanes(states, swimlanesMap)
-    }
-    private fun formatAsSwimlanes(states: List<String>, swimlanesMap: Map<String, List<String>>): String {
-        val builder = StringBuilder()
-
-        for (state in states) {
-            builder.append("$state:\n")
-            val tasks = swimlanesMap[state].orEmpty()
-            if (tasks.isEmpty()) {
-                builder.append("\n")
-            } else {
-                for (task in tasks) {
-                    builder.append("- $task\n")
-                }
-                builder.append("\n")
-            }
-        }
-
-        return builder.toString().trimEnd()
-    }
-
-
 }
