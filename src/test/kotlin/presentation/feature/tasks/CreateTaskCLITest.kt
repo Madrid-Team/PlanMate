8f8189e2a55b160d2cf883f34bec356d7fe1bff6
islam elhady <i.elhady44@gmail.com>
2025-05-02 02:44:00 +0300

    test: add unit tests for CreateProjectCLI for success and fail cases

---
+++ src/test/kotlin/presentation/feature/tasks/CreateTaskCLITest.kt
@@ -0,0 +1,51 @@
+package presentation.feature.tasks
+
+import domain.usecases.project.CreateProjectUseCase
+import domain.utlis.PlanMateExceptions
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import presentation.components.InputReader
+import presentation.components.OutputPrinter
+import presentation.feature.projects.CreateProjectCLI
+import presentation.feature.projects.helperProject
+
+class CreateTaskCLITest {
+    private val inputReader = mockk<InputReader>()
+    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
+    private val useCase = mockk<CreateProjectUseCase>()
+
+    private lateinit var cli: CreateProjectCLI
+
+    @BeforeEach
+    fun setUp() {
+        cli = CreateProjectCLI(inputReader, outputPrinter, useCase)
+    }
+
+
+    @Test
+    fun `should create project successfully when create project then return success`() {
+        every { inputReader.readInput(any()) } returnsMany listOf("project name", "description")
+
+        val project = helperProject(id = "1", name = "project name", description = "description")
+        every { useCase.createProject(project) } returns Result.success(Unit)
+
+        cli.show()
+
+        verify { outputPrinter.printMessage("Project created successfully with ID: 1") }
+    }
+
+    @Test
+    fun `should show error message when creation fails`() {
+        every { inputReader.readInput(any()) } returnsMany listOf("project name", "description")
+
+        val project = helperProject(id = "1", name = "project name", description = "description")
+        every { useCase.createProject(project) } returns Result.failure(PlanMateExceptions("failed"))
+
+        cli.show()
+
+        verify { outputPrinter.printMessage("Failed to create project") }
+    }
+}
