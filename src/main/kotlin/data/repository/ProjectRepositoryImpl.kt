8270b4453407fd84e7774d1dbef542febb630c3e
IslamMagd <eslam752001.magdy54@gmail.com>
2025-05-01 23:05:13 +0300

    add implementation to edit project method in ProjectRepositoryImpl

--- src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
@@ -3,12 +3,31 @@ package data.repository
 import data.source.project.ProjectDataSource
 import data.dto.project.ProjectDto
 import domain.repository.ProjectRepository
+import domain.utlis.PlanMateExceptions
+import domain.utlis.ProjectNotFoundException
 
 class ProjectRepositoryImpl(
     private val projectDataSource: ProjectDataSource
 ):ProjectRepository {
+
+    private lateinit var projects: MutableList<ProjectDto>
+
+    init {
+        initProjects()
+    }
+
+    private fun initProjects() {
+        val result = projectDataSource.getProjects()
+        projects = if (result.isSuccess) {
+            result.getOrThrow().toMutableList()
+        } else {
+            mutableListOf()
+        }
+    }
     override fun getAllProjects(): List<ProjectDto> {
-        TODO("Not yet implemented")
+        return projects.ifEmpty {
+            throw PlanMateExceptions("You haven't any projects yet")
+        }
     }
 
     override fun createProject(project: ProjectDto): Result<Unit> {
@@ -20,8 +39,26 @@ class ProjectRepositoryImpl(
     }
 
     override fun editProject(project: ProjectDto): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+        val updatedProject = projects.find { it.id == project.id }
+        val indexOfUpdatedProject = projects.indexOf(updatedProject)
 
+        return if (updatedProject != null) {
+            try {
+                projects.add(index = indexOfUpdatedProject, updatedProject )
+
+                val result = projectDataSource.editProject(projects)
+
+                if (result.isSuccess) {
+                    Result.success(Unit)
+                } else {
+                    Result.failure(result.exceptionOrNull() ?: Exception("Failed to delete project"))
+                }
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        } else {
+            Result.failure(PlanMateExceptions("Project with ID $updatedProject.id not found"))
+        }
+    }
 
 }
