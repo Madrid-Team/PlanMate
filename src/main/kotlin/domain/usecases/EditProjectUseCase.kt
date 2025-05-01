6e21140264357ca623355c7ea82b82cf293f9e04
IslamMagd <eslam752001.magdy54@gmail.com>
2025-05-01 23:05:52 +0300

    add implementation to edit project method in EditProjectUseCase

--- src/main/kotlin/domain/usecases/EditProjectUseCase.kt
+++ src/main/kotlin/domain/usecases/EditProjectUseCase.kt
@@ -1,11 +1,12 @@
 package domain.usecases
 
+import data.dto.project.ProjectDto
 import domain.models.project.Project
 import domain.repository.ProjectRepository
 
 class EditProjectUseCase(private val projectRepository: ProjectRepository) {
 
-    fun editProject(project: Project):Boolean {
-        return false
+    fun editProject(project: ProjectDto): Result<Unit>{
+        return projectRepository.editProject(project)
     }
 }
