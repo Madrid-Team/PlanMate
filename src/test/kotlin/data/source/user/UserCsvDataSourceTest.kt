5262bc0fe14e74a4e1dc0cada17986b5e054fafd
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-01 17:32:44 +0300

    add three tests to getAllUsers function

--- src/test/kotlin/data/source/user/UserCsvDataSourceTest.kt
+++ src/test/kotlin/data/source/user/UserCsvDataSourceTest.kt
@@ -1,11 +1,14 @@
 package data.source.user
 
+import com.google.common.truth.Truth.assertThat
 import data.dto.authentication.UserDto
 import data.dto.authentication.UserRoleDto
 import data.utils.FileCsvReader
 import data.utils.FileCsvWriter
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class UserCsvDataSourceTest {
 
@@ -35,6 +38,45 @@ class UserCsvDataSourceTest {
         fileCsvReader = mockk()
         userCsvParser = mockk()
         dataSource = UserCsvDataSource(fileCsvReader, fileCsvWriter, userCsvParser)
+    }
+
+    @Test
+    fun `getAllUsers should return all users when result is success`() {
+        // Given
+        every { fileCsvReader.readCsvFile() } returns listOf(row1, row2)
+        every { userCsvParser.parseRowToUser(row1) } returns user1
+        every { userCsvParser.parseRowToUser(row2) } returns user2
+
+        // When
+        val result = dataSource.getAllUsers()
+
+        // Then
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrNull()).containsExactly(user1, user2)
+    }
+
+    @Test
+    fun `getAllUsers should return empty list when no user exist`() {
+        // Given
+        every { fileCsvReader.readCsvFile() } returns listOf()
+
+        // When
+        val result = dataSource.getAllUsers()
+
+        // Then
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrNull()).isEmpty()
+    }
+
+    @Test
+    fun `getAllUsers should throws exception when file read fails`() {
+        // Given
+        every { fileCsvReader.readCsvFile() } throws Exception()
+
+        // When
+        val result = dataSource.getAllUsers()
+        // When & Then
+        assertThat(result.isFailure).isTrue()
     }
 
 }
