8268ee276b9608e2bdee51ba6a673f044f2cedd2
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-05-01 04:13:25 +0300

    improve test cases for create task

--- src/main/kotlin/domain/utlis/TaskExceptions.kt
+++ src/main/kotlin/domain/utlis/TaskExceptions.kt
@@ -2,3 +2,5 @@ package domain.utlis
 
 class TaskNameExistException(error: String = "This task name is already exists") : Exception(error)
 class TaskNotFoundException(error: String = "This task not found") : Exception(error)
+class CannotCreateTaskException(error: String = "Can't create this task") : Exception(error)
+class TaskTitleIsEmptyException(error: String = "Task title is empty") : Exception(error)
--- src/test/kotlin/domain/usecases/CreateTaskUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/CreateTaskUseCaseTest.kt
@@ -3,10 +3,13 @@ package domain.usecases
 import com.google.common.truth.Truth.assertThat
 import createTask
 import domain.repository.TaskRepository
+import domain.utlis.CannotCreateTaskException
+import domain.utlis.TaskTitleIsEmptyException
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 class CreateTaskUseCaseTest {
@@ -22,7 +25,7 @@ class CreateTaskUseCaseTest {
     @Test
     fun `should return true when task is saved successfully`() {
         // given
-        val task = createTask(title = "new task")
+        val task = createTask(title = "new task", description = "description")
         every { taskRepository.createTask(task) } returns true
 
         // when
@@ -37,26 +40,29 @@ class CreateTaskUseCaseTest {
     fun `should return false when task saving fails`() {
         // given
         val task = createTask(title = "new task")
-        every { taskRepository.createTask(task) } returns false
+        every { taskRepository.createTask(task) } throws CannotCreateTaskException()
 
         // when
         val result = createTaskUseCase.createTask(task)
 
         // then
-        verify { taskRepository.createTask(task) }
-        assertThat(result).isFalse()
+        assertThrows<CannotCreateTaskException> {
+            taskRepository.createTask(task)
+        }
     }
 
     @Test
     fun `should return false when task title is empty`() {
         // given
         val task = createTask(title = "")
+        every { taskRepository.createTask(task) } throws TaskTitleIsEmptyException()
 
         // when
         val result = createTaskUseCase.createTask(task)
 
         // then
-        verify(exactly = 0) { taskRepository.createTask(any()) }
-        assertThat(result).isFalse()
+        assertThrows<TaskTitleIsEmptyException> {
+            taskRepository.createTask(task)
+        }
     }
 }
