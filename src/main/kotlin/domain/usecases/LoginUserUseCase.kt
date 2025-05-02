9419ac494976f46575070bff40d02a923f723498
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-02 14:42:39 +0300

    change password string to passwordHash

--- src/main/kotlin/domain/usecases/LoginUserUseCase.kt
+++ src/main/kotlin/domain/usecases/LoginUserUseCase.kt
@@ -2,32 +2,30 @@ package domain.usecases
 
 
 import data.dto.authentication.UserDto
-import data.utils.PasswordHasher
 import domain.repository.UserRepository
 import domain.utlis.UserException
 
 
-class LoginUserUseCase ( private val userRepository: UserRepository   ){
+class LoginUserUseCase(private val userRepository: UserRepository) {
 
-    fun invoke(userName: String?,password: String?): UserDto?{
-        return if (userName.isNullOrEmpty() || password.isNullOrEmpty()) {
-            throw  UserException.NotFoundUser("Not found user or wrong username")
-
-        }else {
-            validateUser(userRepository.getAllUsers().getOrNull(), userName, password)
-        }
-        }
-    private fun validateUser(users: List<UserDto>?, username: String, password: String): UserDto{
-        if (users == null || users.isEmpty()){
+    fun invoke(userName: String?, passwordHash: String?): UserDto? {
+        return if (userName.isNullOrEmpty() || passwordHash.isNullOrEmpty()) {
             throw UserException.NotFoundUser("Not found user or wrong username")
-        }
-        val user=users.find { it.username==username }
-            ?: throw UserException.NotFoundUser("Not found user or wrong username")
-        if(user.passwordHash!= PasswordHasher.hash(password)){
-            throw UserException.WrongPasswordOrUserName("Wrong  password")
-        }
 
-        return user
+        } else {
+            validateUser(userRepository.getAllUsers().getOrNull(), userName, passwordHash)
+        }
     }
 
+    private fun validateUser(users: List<UserDto>?, username: String, passwordHash: String): UserDto {
+        if (users == null || users.isEmpty()) {
+            throw UserException.NotFoundUser("Not found user or wrong username")
+        }
+        val user = users.find { it.username == username }
+            ?: throw UserException.NotFoundUser("Not found user or wrong username")
+        if (user.passwordHash != passwordHash) {
+            throw UserException.WrongPasswordOrUserName("Wrong  password")
+        }
+        return user
+    }
 }
