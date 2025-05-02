73de9d4890cd16995fad44b27ee2200673bdc342
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-02 12:40:08 +0300

    add helperTask function

---
+++ src/test/kotlin/presentation/feature/tasks/helperTask.kt
@@ -0,0 +1,23 @@
+package presentation.feature.tasks
+
+import domain.models.task.Task
+import java.util.*
+
+
+fun helperTask(
+    id: String = UUID.randomUUID().toString(),
+    projectId: String = "",
+    title: String = "",
+    description: String = "",
+    state: String = "",
+    createdBy: String = "",
+    logs: List<String> = listOf(),
+) = Task(
+    id = id,
+    projectId = projectId,
+    title = title,
+    description = description,
+    state = state,
+    createdBy = createdBy,
+    logs = logs
+)
