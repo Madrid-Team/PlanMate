d49d9fc9d5ca414924e5dac8ec2dbcedafbb9e0c
IslamMagd <eslam752001.magdy54@gmail.com>
2025-05-02 04:32:13 +0300

    update logic of EditProjectUseCase

--- src/main/kotlin/domain/usecases/project/EditProjectUseCase.kt
+++ src/main/kotlin/domain/usecases/project/EditProjectUseCase.kt
@@ -3,10 +3,28 @@ package domain.usecases.project
 import data.dto.project.ProjectDto
 import domain.models.project.Project
 import domain.repository.ProjectRepository
+import domain.utlis.*
 
 class EditProjectUseCase(private val projectRepository: ProjectRepository) {
 
-    fun editProject(project: ProjectDto): Result<Unit>{
-        return projectRepository.editProject(project)
+    fun editProject(project: Project): Result<Unit> {
+
+        try {
+            validateName(project)
+            val result = projectRepository.editProject(project)
+            return if (result.isSuccess) {
+                Result.success(Unit)
+            } else {
+                Result.failure(result.exceptionOrNull()!!)
+            }
+        } catch (e: PlanMateExceptions) {
+            return Result.failure(e)
+        }
+    }
+
+    private fun validateName(project: Project) {
+        if (!project.name.matches(Regex("^[A-Za-z ]+$"))) {
+            throw ProjectNameInvalidException()
+        }
     }
 }
