d896375ced6ac1b7e5993f10a6d57a4124003920
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-02 15:35:44 +0300

    add test case for edit task successfully when call edit task

--- src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
+++ src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
@@ -1,26 +1,42 @@
 package presentation.feature.tasks
 
-import domain.usecases.DeleteTaskUseCase
 import domain.usecases.EditTaskUseCase
+import io.mockk.every
 import io.mockk.mockk
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import presentation.components.InputReader
 import presentation.components.OutputPrinter
+import kotlin.test.Test
 
-class EditTaskCLITest{
+class EditTaskCLITest {
 
-  private lateinit var inputReader: InputReader
-  private lateinit var outputPrinter: OutputPrinter
-  private lateinit var editTaskUseCase: EditTaskUseCase
-  private lateinit var taskView: TaskView
-  private lateinit var editTaskCLI: EditTaskCLI
+    private lateinit var inputReader: InputReader
+    private lateinit var outputPrinter: OutputPrinter
+    private lateinit var editTaskUseCase: EditTaskUseCase
+    private lateinit var taskView: TaskView
+    private lateinit var editTaskCLI: EditTaskCLI
 
-  @BeforeEach
-  fun setUp() {
-   inputReader = mockk()
-   outputPrinter = mockk(relaxed = true)
-   editTaskUseCase = mockk()
-   editTaskCLI = EditTaskCLI(inputReader, outputPrinter, taskView, editTaskUseCase)
-  }
- }
+    @BeforeEach
+    fun setUp() {
+        inputReader = mockk()
+        outputPrinter = mockk(relaxed = true)
+        editTaskUseCase = mockk()
+        editTaskCLI = EditTaskCLI(inputReader, outputPrinter, taskView, editTaskUseCase)
+    }
+
+    @Test
+    fun `should edit task successfully when call edit task`() {
+
+        // Given
+        every { inputReader.readInput(any()) } returnsMany listOf("1", "123", "New Title", "New Description")
+        val updatedTask = helperTask(projectId = "1", id = "123", title = "New Title", description = "New Description")
+        every { editTaskUseCase.editTask(updatedTask) } returns true
+
+        // When
+        editTaskCLI.show()
+
+        // Then
+        verify { outputPrinter.printMessage("Task updated successfully") }
+    }
+}
