2d5b9087976874f61bb8f5e2568e4655d71b124e
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-02 17:59:52 +0300

    edit userHeader in appendHeader

--- src/main/kotlin/data/utils/Extensions.kt
+++ src/main/kotlin/data/utils/Extensions.kt
@@ -9,7 +9,7 @@ val String.Companion.user: String get() = "user.csv"
 
 val String.Companion.projectHeader: String get() = "id,name,description,createdBy,projectLogs,projectState,taskStates,projectStates\n"
 val String.Companion.taskHeader: String get() = "id,projectId,title,description,state,createdBy,logs\n"
-val String.Companion.userHeader: String get() = "id,userName,passwordHash,userRole\n"
+val String.Companion.userHeader: String get() = "id,userName,passwordHash,userRole\n1,Mohamed Ashraf,12345678,ADMIN\n"
 
 
 
