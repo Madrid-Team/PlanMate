092c2c536a992c8548a37478896feae7255acec1
Israa Mohamed <e.mohamed@shabek.com>
2025-05-02 03:36:53 +0300

    adding log to projects lists after create new project

---
+++ src/main/kotlin/domain/models/logs/CurrentUser.kt
@@ -0,0 +1,16 @@
+package domain.models.logs
+
+import domain.models.authentication.User
+
+object CurrentUser {
+    private lateinit var user:User
+
+    fun setCurrentUser(user: User) {
+        this.user = user
+    }
+
+    fun getCurrentUser():User {
+        return user
+    }
+
+}
