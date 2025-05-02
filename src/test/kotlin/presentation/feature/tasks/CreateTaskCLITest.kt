63b61067d96fa30828c684b99d28600efa76ee59
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-02 12:13:02 +0300

    add unit test setup for CreateTaskCLI

--- src/test/kotlin/presentation/feature/tasks/CreateTaskCLITest.kt
+++ src/test/kotlin/presentation/feature/tasks/CreateTaskCLITest.kt
@@ -1,6 +1,23 @@
 package presentation.feature.tasks
 
+import domain.usecases.CreateTaskUseCase
+import domain.usecases.project.CreateProjectUseCase
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
-class CreateTaskCLITest(){
+import org.junit.jupiter.api.BeforeEach
+import presentation.components.InputReader
+import presentation.components.OutputPrinter
+import presentation.feature.projects.CreateProjectCLI
 
+class CreateTaskCLITest(){
+    private val inputReader = mockk<InputReader>()
+    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
+    private val useCase = mockk<CreateTaskUseCase>()
+    private val taskView = mockk<TaskView>()
+    private lateinit var cli : CreateTaskCLI
+
+    @BeforeEach
+    fun setUp() {
+        cli = CreateTaskCLI(inputReader, outputPrinter,taskView, useCase)
+    }
 }
