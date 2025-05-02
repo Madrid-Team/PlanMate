22b807a6056b48e581963e4a7afe2f3a64d304b5
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-02 14:43:59 +0300

    add unit test setup for EditTaskCLI

--- src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
+++ src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
@@ -1,6 +1,26 @@
 package presentation.feature.tasks
 
+import domain.usecases.DeleteTaskUseCase
+import domain.usecases.EditTaskUseCase
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
- class EditTaskCLITest{
+import org.junit.jupiter.api.BeforeEach
+import presentation.components.InputReader
+import presentation.components.OutputPrinter
 
+class EditTaskCLITest{
+
+  private lateinit var inputReader: InputReader
+  private lateinit var outputPrinter: OutputPrinter
+  private lateinit var editTaskUseCase: EditTaskUseCase
+  private lateinit var taskView: TaskView
+  private lateinit var editTaskCLI: EditTaskCLI
+
+  @BeforeEach
+  fun setUp() {
+   inputReader = mockk()
+   outputPrinter = mockk(relaxed = true)
+   editTaskUseCase = mockk()
+   editTaskCLI = EditTaskCLI(inputReader, outputPrinter, taskView, editTaskUseCase)
+  }
  }
