e34d7db344810e8732886382134e797421521b52
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-02 20:26:42 +0300

    add implementation UserCLI

--- src/main/kotlin/presentation/feature/user/UserCLI.kt
+++ src/main/kotlin/presentation/feature/user/UserCLI.kt
@@ -10,7 +10,18 @@ class UserCLI(
     private val deleteUserCLI: DeleteUserCLI
 ) {
     fun show() {
-
+        while (true) {
+            outputPrinter.printMessage("=== Manage users ===")
+            outputPrinter.printMessage("1. Create new user")
+            outputPrinter.printMessage("2. Delete new user")
+            outputPrinter.printMessage("0. Back")
+            when (inputReader.readInput("Select an option: ")) {
+                "1" -> createUserCLI.show()
+                "2" -> deleteUserCLI.show()
+                "0" -> return
+                else -> outputPrinter.printError("Invalid option.")
+            }
+        }
     }
 
 }
