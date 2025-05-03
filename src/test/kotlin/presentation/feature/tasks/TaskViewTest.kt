9fdd2cd90302d555e9cc09326c6e9194b0b60b90
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-03 10:31:33 +0300

    add test case for print project not found when project does not exist

--- src/test/kotlin/presentation/feature/tasks/TaskViewTest.kt
+++ src/test/kotlin/presentation/feature/tasks/TaskViewTest.kt
@@ -1,6 +1,41 @@
 package presentation.feature.tasks
 
+import domain.usecases.DisplayAllTasksUseCase
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import presentation.components.InputReader
+import presentation.components.OutputPrinter
+import org.junit.jupiter.api.Test
+import io.mockk.verify
+import io.mockk.every
+
+
 class TaskViewTest(){
+    private lateinit var displayAllTasksUseCase: DisplayAllTasksUseCase
+    private lateinit var inputReader: InputReader
+    private lateinit var outputPrinter: OutputPrinter
+    private lateinit var taskView: TaskView
+
+    @BeforeEach
+    fun setUp() {
+        inputReader = mockk()
+        outputPrinter = mockk(relaxed = true)
+        displayAllTasksUseCase = mockk()
+        taskView = TaskView(displayAllTasksUseCase, outputPrinter, inputReader)
+    }
+    @Test
+    fun `should print project not found when project does not exist`() {
+        // Given
+        val projectId = "non_existing_project"
+        every { inputReader.readInput("Enter project ID: ") } returns projectId
+        every { displayAllTasksUseCase.display(projectId) } returns "Project not found."
+
+        // When
+        taskView.show()
+
+        // Then
+        verify { outputPrinter.printMessage("Project not found.") }
+    }
 
 }
