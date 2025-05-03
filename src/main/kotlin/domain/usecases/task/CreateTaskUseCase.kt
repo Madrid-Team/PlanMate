adb73ddd8205d2482ebeeb57c7e1b7012993875f
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-05-03 07:25:22 +0300

    change create task use case logic

--- src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
@@ -2,16 +2,24 @@ package domain.usecases.task
 
 import domain.models.task.Task
 import domain.repository.TaskRepository
+import domain.usecases.task.validation.CheckTaskValidationUseCase
+import domain.utlis.CannotCreateTaskException
 
 class CreateTaskUseCase(
-    private val taskRepository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val checkTaskValidationUseCase: CheckTaskValidationUseCase
 ) {
-    fun createTask(task: Task): Boolean {
+    fun createTask(task: Task): Result<Unit> {
         try {
-            taskRepository.createTask(task)
-        } catch (e: Exception) {
-            println("Can't create task")
+            checkTaskValidationUseCase(task)
+            val result = taskRepository.createTask(task)
+            return if (result.isSuccess) {
+                Result.success(Unit)
+            } else {
+                Result.failure(result.exceptionOrNull()!!)
+            }
+        } catch (e: CannotCreateTaskException) {
+            return Result.failure(e)
         }
-        return true
     }
 }
