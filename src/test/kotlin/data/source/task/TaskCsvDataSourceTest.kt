c60ec7736cd722a71c992fb9bb0d68b71ec26757
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-05-04 08:15:25 +0300

    update data source test cases for task

--- src/test/kotlin/data/source/task/TaskCsvDataSourceTest.kt
+++ src/test/kotlin/data/source/task/TaskCsvDataSourceTest.kt
@@ -1,21 +1,15 @@
 package data.source.task
 
 import com.google.common.truth.Truth.assertThat
-import data.dto.project.ProjectDto
-import data.mapper.toDto
 import data.utils.FileCsvReader
 import data.utils.FileCsvWriter
-import data.utils.taskHeader
-import domain.models.logs.CreatedLogFormatter
-import domain.models.logs.EntityType
-import domain.usecases.task.createTask
-import domain.utlis.TaskExceptions
-import io.mockk.*
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import java.io.IOException
-import java.util.UUID
 import kotlin.test.assertTrue
 
 class TaskCsvDataSourceTest {
@@ -33,7 +27,7 @@ class TaskCsvDataSourceTest {
     }
 
     @Test
-    fun `should return success result when tasks are edited successfully`() {
+    fun `editTask should return success when file updated successfully`() {
         val tasks = listOf(
             helperTaskDto(title = "task1", description = "description one"),
             helperTaskDto(title = "task2", description = "description two")
@@ -51,20 +45,22 @@ class TaskCsvDataSourceTest {
 
 
         assertThat(result.isSuccess).isTrue()
-        verify { fileCsvWriter.updateCsvFile(String.taskHeader + parsedTasks.joinToString("")) }
     }
 
 
-//    @Test
-//    fun `should edit task throw exception when failed to edit the task`() {
-//        assertThrows<TaskNotFoundException> {
-//            taskDataSource.editTask(task)
-//        }
-//    }
+    @Test
+    fun `editTask should return failure when failed to update file`() {
+        every { taskCsvParser.parseTaskToString(task) } returns csvRow
+        every { fileCsvWriter.updateCsvFile(csvRow) } throws IOException()
+
+        val result = taskDataSource.editTask(tasksDto)
+
+        assertTrue { result.isFailure }
+    }
 
     @Test
-    fun `should delete task return true when task id is found`() {
-        every { taskCsvParser.parseTaskToString(task.toDto()) } returns csvRow
+    fun `deleteTask should return failure when failed to update file`() {
+        every { taskCsvParser.parseTaskToString(task) } returns csvRow
         every { fileCsvWriter.updateCsvFile(any()) } returns Unit
 
         val result = taskDataSource.deleteTask(tasksDto)
@@ -72,53 +68,43 @@ class TaskCsvDataSourceTest {
         assertTrue { result.isSuccess }
     }
 
-//    @Test
-//    fun `should delete task throw exception when task id is not found`() {
-//        every { taskCsvParser.parseTaskToString(task.toDto()) } returns csvRow
-//        every { fileCsvWriter.updateCsvFile(any()) } returns Unit
-//
-//        val result = taskDataSource.deleteTask(listOf(task.toDto()))
-//
-//        assertTrue { result.isFailure }
-//    }
-
 
     @Test
-    fun `should create task return true when creating task successfully`() {
-        every { taskCsvParser.parseTaskToString(task.toDto()) } returns csvRow
+    fun `createTask should return success when creating task successfully`() {
+        every { taskCsvParser.parseTaskToString(task) } returns csvRow
         every { fileCsvWriter.writeToCsvFile(csvRow) } returns Unit
 
-        val result = taskDataSource.createTask(task.toDto())
+        val result = taskDataSource.createTask(task)
 
         assertTrue { result.isSuccess }
     }
 
     @Test
-    fun `should create task throw exception when creating task is failed`() {
-        every { taskCsvParser.parseTaskToString(task.toDto()) } returns csvRow
+    fun `createTask should return failure when writing to csv file is failed`() {
+        every { taskCsvParser.parseTaskToString(task) } returns csvRow
         every { fileCsvWriter.writeToCsvFile(csvRow) } throws IOException()
 
-        val result = taskDataSource.createTask(task.toDto())
+        val result = taskDataSource.createTask(task)
 
         assertTrue { result.isFailure }
     }
 
     @Test
-    fun `should get all tasks return list of tasks`() {
+    fun `getAllTasks should return list of tasks when read from file successfully`() {
         every { fileCsvReader.readCsvFile() } returns listOf("task", "task2")
-        every { taskCsvParser.parseOneRowToTask("task") } returns task.toDto()
-        every { taskCsvParser.parseOneRowToTask("task2") } returns task.toDto()
+        every { taskCsvParser.parseOneRowToTask("task") } returns task
+        every { taskCsvParser.parseOneRowToTask("task2") } returns task
 
         val result = taskDataSource.getAllTasks()
 
-        assertThat(result.getOrNull()).isEqualTo(listOf(task.toDto(), task.toDto()))
+        assertThat(result.getOrNull()).isEqualTo(listOf(task, task))
     }
 
     @Test
-    fun `getTaskByProjectId should return list of tasks when id is found`() {
+    fun `getTaskByProjectId should return list of tasks when list of tasks is not empty`() {
         every { fileCsvReader.readCsvFile() } returns listOf("task1", "task2")
-        every { taskCsvParser.parseOneRowToTask("task1") } returns task.toDto().copy(projectId = projectId)
-        every { taskCsvParser.parseOneRowToTask("task2") } returns task.toDto().copy(projectId = projectId)
+        every { taskCsvParser.parseOneRowToTask("task1") } returns task.copy(projectId = projectId)
+        every { taskCsvParser.parseOneRowToTask("task2") } returns task.copy(projectId = projectId)
 
         val result = taskDataSource.getTasksByProjectId(projectId)
 
@@ -126,10 +112,10 @@ class TaskCsvDataSourceTest {
     }
 
     @Test
-    fun `getTaskByProjectId should return empty list when id is not found`() {
+    fun `getTaskByProjectId should return empty list when list of tasks is empty`() {
         every { fileCsvReader.readCsvFile() } returns listOf("task1", "task2")
-        every { taskCsvParser.parseOneRowToTask("task1") } returns task.toDto().copy(projectId = "id1")
-        every { taskCsvParser.parseOneRowToTask("task2") } returns task.toDto().copy(projectId = "id2")
+        every { taskCsvParser.parseOneRowToTask("task1") } returns task.copy(projectId = "id1")
+        every { taskCsvParser.parseOneRowToTask("task2") } returns task.copy(projectId = "id2")
 
         val result = taskDataSource.getTasksByProjectId(projectId)
 
@@ -137,14 +123,11 @@ class TaskCsvDataSourceTest {
     }
 
     companion object {
-        val task = createTask(id = "08a823be-fc8f-4310-9bcb-543d577ebb93", title = "task")
+        val task = helperTaskDto(id = "08a823be-fc8f-4310-9bcb-543d577ebb93", title = "task")
         const val csvRow = "08a823be-fc8f-4310-9bcb-543d577ebb93,task"
-        val taskId = task.id
-        val projectId = "08a823be-fc8f-4310-9bcb-543d577ebb93"
+        const val projectId = "08a823be-fc8f-4310-9bcb-543d577ebb93"
         val tasksDto = listOf(
             helperTaskDto(id = "08a823be-fc8f-4310-9bcb-543d577ebb93", title = "task"),
         )
-//            helperTaskDto(id = "08a823be-fc8f-4310-9bcb-543d577ebb93", title = "test2")
-
     }
 }
