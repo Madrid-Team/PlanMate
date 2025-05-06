a3699cf06d94a1cf2f81844dea764bca6009dd85
Abdelrahmanibrrahem <ai4516299@gmail.com>
2025-05-06 17:54:12 +0300

    add testcase for MongoClientProvider to check connection and database is found

---
+++ src/test/kotlin/data/mongoDB/MongoClientProviderTest.kt
@@ -0,0 +1,41 @@
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import kotlinx.coroutines.runBlocking
+import org.bson.Document
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.madrid.data.source.mongoDb.MongoClientProvider
+import org.madrid.data.utils.DATABASE_NAME
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MongoClientProviderTest {
+
+    private lateinit var mongoClientProvider: MongoClientProvider
+    private lateinit var database: MongoDatabase
+
+    @BeforeAll
+    fun setup() {
+        mongoClientProvider = MongoClientProvider()
+        database = mongoClientProvider.database
+    }
+
+    @Test
+    fun `Should provide a valid MongoDatabase instance When connection is correct and database is returned`() {
+        assertNotNull(database)
+        assertEquals(DATABASE_NAME, database.name) // Replace with your actual DB name
+    }
+
+    @Test
+    fun `Should respond to ping command`() = runBlocking {
+        val result: Document =
+            assertDoesNotThrow { database.runCommand(Document("ping", 1)) }
+
+        assertEquals(1, result.getInteger("ok"))
+
+    }
+
+}
