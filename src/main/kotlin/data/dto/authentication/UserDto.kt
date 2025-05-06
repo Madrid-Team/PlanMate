289908c5c2edb913ccf5416b9840fc20ed35c385
Israa Mohamed <e.mohamed@shabek.com>
2025-05-06 04:47:29 +0300

    add required annotations in data models for mongo db

--- src/main/kotlin/data/dto/authentication/UserDto.kt
+++ src/main/kotlin/data/dto/authentication/UserDto.kt
@@ -1,7 +1,12 @@
 package data.dto.authentication
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+
+@Serializable
 data class UserDto(
-    val id: String,
+    @BsonId @SerialName("_id") val id: String,
     val username: String,
     val passwordHash: String,
     val role: String,
--- src/main/kotlin/data/dto/project/ProjectDto.kt
+++ src/main/kotlin/data/dto/project/ProjectDto.kt
@@ -1,9 +1,15 @@
 package data.dto.project
 
+import data.dto.task.TaskDto
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
 import java.util.*
 
+
+@Serializable
 data class ProjectDto(
-    val id: String,
+    @BsonId @SerialName("_id") val id: String,
     val name: String,
     val description: String,
     val createdBy: String,
@@ -11,4 +17,6 @@ data class ProjectDto(
     val projectState: String,
     val taskStates: List<String>,
     val projectStates: List<String>,
+    val matesIds:List<String> = emptyList(),
+    val tasks:List<TaskDto> = emptyList()
 )
