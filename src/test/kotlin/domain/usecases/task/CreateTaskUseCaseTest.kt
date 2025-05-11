30436ef49dc0c6c1f16677c83b4a84da93a82759
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-05-11 15:52:30 +0300

    update create task use case to give coverage 100%

--- src/test/kotlin/domain/usecases/task/CreateTaskUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/task/CreateTaskUseCaseTest.kt
@@ -4,8 +4,8 @@ import domain.repository.TaskRepository
 import domain.utils.TaskExceptions.TaskTitleIsEmptyException
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -25,7 +25,7 @@ class CreateTaskUseCaseTest {
 
     @Test
     fun `createTask should execute successfully when task is saved successfully`() {
-        testScope.launch {
+        testScope.runTest {
             val task = createTask(title = "new task", description = "description")
             coEvery { taskRepository.createTask(task) } returns Unit
 
@@ -35,7 +35,7 @@ class CreateTaskUseCaseTest {
 
     @Test
     fun `createTask should throw exception when task saving fails`() {
-        testScope.launch {
+        testScope.runTest {
             // given
             val task = createTask(title = "new task")
             coEvery { taskRepository.createTask(task) } throws Exception()
@@ -49,7 +49,7 @@ class CreateTaskUseCaseTest {
 
     @Test
     fun `createTask should return false when task title is empty`() {
-       testScope.launch {
+       testScope.runTest {
            // given
            val task = createTask(title = "")
            coEvery { taskRepository.createTask(task) } throws TaskTitleIsEmptyException()
