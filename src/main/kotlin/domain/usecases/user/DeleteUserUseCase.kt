990ca6246a78e2e9605b17f8fd45b345f575a87e
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-12 18:42:25 +0300

    Revert "refactor DeleteUserUseCase logic"
    This reverts commit d6072aac1760328426ace0e80518431c19153a63.

--- src/main/kotlin/domain/usecases/user/DeleteUserUseCase.kt
+++ src/main/kotlin/domain/usecases/user/DeleteUserUseCase.kt
@@ -3,7 +3,6 @@ package domain.usecases.user
 import domain.models.authentication.UserRole
 import domain.repository.UserRepository
 import domain.utils.UserExceptions
-import domain.usecases.user.ValidateUser
 
 class DeleteUserUseCase(
     private val userRepository: UserRepository
@@ -11,6 +10,10 @@ class DeleteUserUseCase(
     suspend fun deleteUser(userRequestId: String, userToDeleteId: String) {
         val user = userRepository.getUserById(userRequestId)
         userRepository.getUserById(userToDeleteId)
-        ValidateUser(userRepository).validateUserRoleToDelete(user.role,userToDeleteId)
+        if (user.role == UserRole.ADMIN.name) {
+            userRepository.deleteUser(userToDeleteId)
+        } else {
+            throw UserExceptions.UserNotAdminException()
+        }
     }
 }
