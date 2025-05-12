b98a8961515be2a391a1dd36ccafe1e6206a32c5
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-12 11:54:56 +0300

    remove hard coded string

---
+++ src/main/kotlin/presentation/utils/Constants.kt
@@ -0,0 +1,11 @@
+package presentation.utils
+
+val String.Companion.selectOption get() = "Select an option: "
+val String.Companion.exit get() = "0. Exit"
+val String.Companion.welcomeMessage get() = "=== Welcome to PlanMate ==="
+val String.Companion.mainMenu get() = "=== === Main Menu === ==="
+val String.Companion.login get() = "1. Log in"
+val String.Companion.goodbye get() = "Goodbye!"
+val String.Companion.invalidOption get() = "Invalid option."
+val String.Companion.selectionOne get() = "1"
+val String.Companion.selectionZero get() = "0"
