db97b381cac07e83149aef35bcf211cd083a7a00
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-12 12:45:35 +0300

    refactor edit task use case logic

--- src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
@@ -2,11 +2,27 @@ package domain.usecases.task
 
 import domain.models.task.Task
 import domain.repository.TaskRepository
+import domain.usecases.project.GetProjectByIdUseCase
+import domain.utils.TaskExceptions
 
 class EditTaskUseCase(
-    private val taskRepository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val getProjectByIdUseCase: GetProjectByIdUseCase,
+    private val getTaskByIdUseCase: GetTaskByIdUseCase,
 ) {
-    suspend operator fun invoke(task: Task) {
+    suspend fun editTask(task: Task) {
+        // Verify project exists
+        getProjectByIdUseCase.getById(task.projectId)
+
+        // Verify task exists
+        getTaskByIdUseCase.getTaskById(task.projectId, task.id.toString())
+
+        if (task.title.isEmpty()) {
+            throw TaskExceptions.TaskTitleIsEmptyException()
+        }
+        if (task.description.isEmpty()) {
+            throw TaskExceptions.TaskDescriptionIsEmptyException()
+        }
         taskRepository.editTask(task)
     }
 }
