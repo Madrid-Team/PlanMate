320457a8ac8f4a52541d605f7b0dd1046e335e07
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-12 15:00:14 +0300

    refactor edit task use case logic after adding TaskValidator class

--- src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
@@ -3,12 +3,12 @@ package domain.usecases.task
 import domain.models.task.Task
 import domain.repository.TaskRepository
 import domain.usecases.project.GetProjectByIdUseCase
-import domain.utils.TaskExceptions
 
 class EditTaskUseCase(
     private val taskRepository: TaskRepository,
     private val getProjectByIdUseCase: GetProjectByIdUseCase,
     private val getTaskByIdUseCase: GetTaskByIdUseCase,
+    private val taskValidator: TaskValidator
 ) {
     suspend fun editTask(task: Task) {
         // Verify project exists
@@ -17,12 +17,7 @@ class EditTaskUseCase(
         // Verify task exists
         getTaskByIdUseCase.getTaskById(task.projectId, task.id.toString())
 
-        if (task.title.isEmpty()) {
-            throw TaskExceptions.TaskTitleIsEmptyException()
-        }
-        if (task.description.isEmpty()) {
-            throw TaskExceptions.TaskDescriptionIsEmptyException()
-        }
+        taskValidator.validateBasic(task)
         taskRepository.editTask(task)
     }
 }
