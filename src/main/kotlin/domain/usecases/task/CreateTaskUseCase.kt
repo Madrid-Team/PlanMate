7ad5c5d70b706cf52e6bcf26ddcdb32dc98e77da
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-12 11:55:50 +0300

    add create task use case logic

--- src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
@@ -2,11 +2,29 @@ package domain.usecases.task
 
 import domain.models.task.Task
 import domain.repository.TaskRepository
+import domain.usecases.project.GetProjectByIdUseCase
+import domain.utils.ProjectExceptions
+import domain.utils.TaskExceptions
 
 class CreateTaskUseCase(
-    private val taskRepository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val getProjectByIdUseCase: GetProjectByIdUseCase
+
 ) {
-    suspend operator fun invoke(task: Task) {
+    suspend fun createTask(task: Task) {
+        if (task.projectId.isBlank()) {
+            throw ProjectExceptions.ProjectNotFoundException()
+        }
+        getProjectByIdUseCase.getById(task.projectId)
+        if (task.title.isEmpty()) {
+            throw TaskExceptions.TaskTitleIsEmptyException()
+        }
+        if (task.description.isEmpty()) {
+            throw TaskExceptions.TaskDescriptionIsEmptyException()
+        }
+        if (task.taskState.isEmpty()) {
+            throw TaskExceptions.TaskStateIsEmptyException()
+        }
         taskRepository.createTask(task)
     }
 }
