8c49987967ba9ee2bc1198dc73d645eb6600c00e
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-12 20:19:17 +0300

    add project validator class

---
+++ src/main/kotlin/domain/usecases/project/ProjectValidator.kt
@@ -0,0 +1,32 @@
+package domain.usecases.project
+
+import domain.models.project.Project
+import domain.utils.ProjectExceptions
+
+class ProjectValidator {
+    fun validate(project: Project) {
+        validateName(project)
+
+        if (project.description.isBlank()) {
+            throw ProjectExceptions.ProjectDescriptionIsEmptyException()
+        }
+
+        if (project.projectStates.isEmpty()) {
+            throw ProjectExceptions.ProjectStatesIsEmptyException()
+        }
+
+        if (project.taskStates.isEmpty()) {
+            throw ProjectExceptions.ProjectTaskStatesIsEmptyException()
+        }
+    }
+
+    fun validateName(project: Project) {
+        if (project.name.isBlank()) {
+            throw ProjectExceptions.ProjectNameInvalidException()
+        }
+
+        if (!project.name.matches(Regex("^[A-Za-z ]+$"))) {
+            throw ProjectExceptions.ProjectNameInvalidException()
+        }
+    }
+}
