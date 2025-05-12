bbc4c61ba6276c4eb17baf50a84c23e84294bfe0
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-12 15:00:31 +0300

    refactor create task use case logic after adding TaskValidator class

--- src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/CreateTaskUseCase.kt
@@ -4,27 +4,17 @@ import domain.models.task.Task
 import domain.repository.TaskRepository
 import domain.usecases.project.GetProjectByIdUseCase
 import domain.utils.ProjectExceptions
-import domain.utils.TaskExceptions
 
 class CreateTaskUseCase(
     private val taskRepository: TaskRepository,
-    private val getProjectByIdUseCase: GetProjectByIdUseCase
+    private val getProjectByIdUseCase: GetProjectByIdUseCase,
+    private val taskValidator: TaskValidator
 
 ) {
     suspend fun createTask(task: Task) {
-        if (task.projectId.isBlank()) {
-            throw ProjectExceptions.ProjectNotFoundException()
-        }
         getProjectByIdUseCase.getById(task.projectId)
-        if (task.title.isEmpty()) {
-            throw TaskExceptions.TaskTitleIsEmptyException()
-        }
-        if (task.description.isEmpty()) {
-            throw TaskExceptions.TaskDescriptionIsEmptyException()
-        }
-        if (task.taskState.isEmpty()) {
-            throw TaskExceptions.TaskStateIsEmptyException()
-        }
+        taskValidator.validateAll(task)
+
         taskRepository.createTask(task)
     }
 }
