3f3d8717830a073ee357dabaabb3af2b9d0254f9
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-13 02:02:22 +0300

    refactor createUserUseCase logic

--- src/main/kotlin/domain/usecases/user/CreateUserUseCase.kt
+++ src/main/kotlin/domain/usecases/user/CreateUserUseCase.kt
@@ -3,13 +3,24 @@ package domain.usecases.user
 import domain.models.authentication.User
 import domain.repository.UserRepository
 import domain.usecases.user.ValidateUser
+import domain.utils.NameValidationResult
+import domain.utils.PasswordValidationResult
+import domain.utils.UserExceptions
 
 class CreateUserUseCase(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val validateNameUseCase: ValidateNameUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase
 ) {
-    suspend  fun createUser(user: User) {
-        val userId = ValidateUser(userRepository).generateUUIDValidToNewUser()
-        val newUser = user.copy(id = userId)
-        userRepository.createNewUser(newUser)
-    }
+    suspend fun createUser(user: User) {
+        val nameValidation = validateNameUseCase.validateName(user.username)
+        if (nameValidation is NameValidationResult.NotValid) {
+            throw UserExceptions.InvalidUserName()
+        }
+        val passwordValidation = validatePasswordUseCase.validatePassword(user.passwordHash)
+        if (passwordValidation is PasswordValidationResult.NotValid) {
+            throw UserExceptions.InvalidPasswordError()
+        }
+        userRepository.createNewUser(user)
 }
+    }
