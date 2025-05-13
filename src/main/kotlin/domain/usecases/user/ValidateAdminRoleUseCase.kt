73b4acbfcd334bfc5112f95ed5e7bf5467f18a94
khater <khater@email.com>
2025-05-13 17:40:23 +0300

    make validate admin role use case throw exception instead of Result

--- src/main/kotlin/domain/usecases/user/ValidateAdminRoleUseCase.kt
+++ src/main/kotlin/domain/usecases/user/ValidateAdminRoleUseCase.kt
@@ -6,11 +6,7 @@ import domain.utils.AdminValidationResult
 
 
 class ValidateAdminRoleUseCase() {
-     fun validate( role: String): AdminValidationResult {
-        return if (role == UserRole.ADMIN.name) {
-            AdminValidationResult.Valid
-        } else {
-            AdminValidationResult.NotAdmin
-        }
+     fun validate(role: String): Boolean {
+         return role == UserRole.ADMIN.name
     }
 }
