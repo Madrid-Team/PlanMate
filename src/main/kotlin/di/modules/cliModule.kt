5e2da712042026ae2ded33de17b9dbf8612cc0df
khater <khater@email.com>
2025-05-13 03:47:39 +0300

    update cli modules with new classes

--- src/main/kotlin/di/modules/cliModule.kt
+++ src/main/kotlin/di/modules/cliModule.kt
@@ -19,21 +19,21 @@ val cliModule = module {
     single<InputReader> { ConsoleInputReader() }
     single<OutputPrinter> { ConsoleOutputPrinter() }
 
-    single { CreateTaskCLI(get(), get(), get(), get() ) }
+    single { CreateTaskCLI(get(), get(), get(), get(), get() ) }
     single { DeleteTaskCLI(get(), get(), get()) }
-    single { EditTaskCLI(get(), get(), get()) }
+    single { EditTaskCLI(get(), get(), get(),get()) }
     single { TaskCLI(get(), get(), get(), get(), get(), get(), get()) }
     single { ProjectAuditLogCLI(get(), get(), get()) }
     single { TaskAuditLogCLI(get(), get(), get()) }
     single { AuthenticationCLI(get(), get(), get()) }
-    single { PlanMateCLI(get(), get(), get(), get(), get()) }
+    single { PlanMateCLI(get(), get(), get(), get(), get() ) }
     single { ProjectCLI(get(), get(), get(), get(), get(), get(), get(), get()) }
-    single { CreateProjectCLI(get(), get(), get() ) }
+    single { CreateProjectCLI(get(), get(), get(),get() ) }
     single { DeleteProjectCLI(get(), get(), get()) }
     single { EditProjectCLI(get(), get(), get(), get()) }
     single { UserCLI(get(), get(), get(), get()) }
     single { CreateUserCLI(get(), get(), get()) }
-    single { DeleteUserCLI(get(), get(), get()) }
+    single { DeleteUserCLI(get(), get(), get(), get()) }
     single { ProjectView(get()) }
     single { AdminCLI(get(), get(), get(), get(), get()) }
     single { MateCLI(get(), get(), get(), get()) }
