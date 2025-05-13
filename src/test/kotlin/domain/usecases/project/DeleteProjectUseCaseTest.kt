2497013babf824be8bfea68937eb881bad71824d
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-13 17:27:21 +0300

    refactor DeleteProjectUseCaseTest

--- src/test/kotlin/domain/usecases/project/DeleteProjectUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/project/DeleteProjectUseCaseTest.kt
@@ -3,6 +3,7 @@ package domain.usecases.project
 import domain.repository.ProjectRepository
 import domain.utils.ProjectExceptions
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
@@ -23,31 +24,76 @@ class DeleteProjectUseCaseTest {
     }
 
     @Test
-    fun `should delete project when it exists`() {
+    fun `should delete project when data is valid`() {
         runTest {
-            //Given
+            // Given
             val projectId = "project-123"
             coEvery { getProjectByIdUseCase.getById(projectId) } returns mockk()
             coEvery { projectRepository.deleteProject(projectId) } returns Unit
 
-            //When
+            // When
             assertDoesNotThrow {
                 deleteProjectUseCase.deleteProject(projectId)
             }
+
+            // Then
+            coVerify(exactly = 1) { getProjectByIdUseCase.getById(projectId) }
+            coVerify(exactly = 1) { projectRepository.deleteProject(projectId) }
         }
     }
 
     @Test
-    fun `should throw exception when project does not exist`() {
+    fun `should throw ProjectNotFoundException when project does not exist and not call delete`() {
         runTest {
-            //Given
+            // Given
             val projectId = "not-found"
             coEvery { getProjectByIdUseCase.getById(projectId) } throws ProjectExceptions.ProjectNotFoundException()
 
-            //When
+            // When & Then
             assertThrows<ProjectExceptions.ProjectNotFoundException> {
                 deleteProjectUseCase.deleteProject(projectId)
             }
+
+            // Then
+            coVerify(exactly = 1) { getProjectByIdUseCase.getById(projectId) }
+            coVerify(exactly = 0) { projectRepository.deleteProject(any()) }
         }
     }
+
+    @Test
+    fun `should throw ProjectNotFoundException when projectId is blank`() {
+        runTest {
+            // Given
+            val blankId = ""
+            coEvery { getProjectByIdUseCase.getById(blankId) } throws ProjectExceptions.ProjectNotFoundException("Project ID cannot be empty")
+
+            // When & Then
+            assertThrows<ProjectExceptions.ProjectNotFoundException> {
+                deleteProjectUseCase.deleteProject(blankId)
+            }
+
+            coVerify(exactly = 1) { getProjectByIdUseCase.getById(blankId) }
+            coVerify(exactly = 0) { projectRepository.deleteProject(any()) }
+        }
+    }
+
+    @Test
+    fun `should throw exception when deleteProject throws unexpected error`() {
+        runTest {
+            // Given
+            val projectId = "existing-id"
+            coEvery { getProjectByIdUseCase.getById(projectId) } returns mockk()
+            coEvery { projectRepository.deleteProject(projectId) } throws RuntimeException("DB error")
+
+            // When & Then
+            assertThrows<RuntimeException> {
+                deleteProjectUseCase.deleteProject(projectId)
+            }
+
+            coVerify(exactly = 1) { getProjectByIdUseCase.getById(projectId) }
+            coVerify(exactly = 1) { projectRepository.deleteProject(projectId) }
+        }
+    }
+
+
 }
