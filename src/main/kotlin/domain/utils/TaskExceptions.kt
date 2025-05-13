f2aa210a16e3bbe15e0919617d2f7fd76853be29
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-13 17:29:00 +0300

    add new exception

--- src/main/kotlin/domain/utils/TaskExceptions.kt
+++ src/main/kotlin/domain/utils/TaskExceptions.kt
@@ -7,5 +7,6 @@ open class TaskExceptions(message: String) : PlanMateExceptions(message) {
     class TaskDescriptionIsEmptyException(error: String = "Task description is empty") : TaskExceptions(error)
     class TaskStateIsEmptyException(error: String = "Task State is empty") : TaskExceptions(error)
     class TaskCannotEditException(error: String = "Can't edit this task") : TaskExceptions(error)
+    class TaskEditFailedException(error: String = "Failed to edit task") : TaskExceptions(error)
     class NoLogsFoundException(error: String = "There are no logs found for this task") : TaskExceptions(error)
 }
