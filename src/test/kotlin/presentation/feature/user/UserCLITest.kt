dc7bcab168460a5a5fef889059b86ee5fa3291da
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-13 17:21:04 +0300

    fix tests

--- src/test/kotlin/presentation/feature/user/UserCLITest.kt
+++ src/test/kotlin/presentation/feature/user/UserCLITest.kt
@@ -9,95 +9,119 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import presentation.components.InputReader
 import presentation.components.OutputPrinter
+import presentation.utils.back
+import presentation.utils.createNewUser
+import presentation.utils.deleteUser
+import presentation.utils.invalidOption
+import presentation.utils.manageUsersMenu
+import presentation.utils.selectOption
 
-//class UserCLITest {
-//    private val inputReader = mockk<InputReader>()
-//    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
-//    private val createUserCLI = mockk<CreateUserCLI>(relaxed = true)
-//    private val deleteUserCLI = mockk<DeleteUserCLI>(relaxed = true)
-//    private lateinit var cli: UserCLI
-//
-//    @BeforeEach
-//    fun setUp() {
-//        cli = UserCLI(inputReader, outputPrinter, createUserCLI, deleteUserCLI)
-//    }
-//
-//    @Test
-//    fun `should show menu and create user when enter 1`() {
-//        runTest {
-//            // Given
-//            every { inputReader.readInput("Select an option: ") } returns "1" andThen "0"
-//
-//            // When
-//            cli.show()
-//
-//            // Then
-//            coVerify {
-//                outputPrinter.printMessage("=== Manage users ===")
-//                outputPrinter.printMessage("1. Create new user")
-//                outputPrinter.printMessage("2. Delete new user")
-//                outputPrinter.printMessage("0. Back")
-//                createUserCLI.show()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `should show menu and delete user when enter 2`() {
-//        runTest {
-//            // Given
-//            every { inputReader.readInput("Select an option: ") } returns "2" andThen "0"
-//
-//            // When
-//            cli.show()
-//
-//            // Then
-//            coVerify {
-//                outputPrinter.printMessage("=== Manage users ===")
-//                outputPrinter.printMessage("1. Create new user")
-//                outputPrinter.printMessage("2. Delete new user")
-//                outputPrinter.printMessage("0. Back")
-//                deleteUserCLI.show()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `should back when enter 0`() {
-//        runTest {
-//            // Given
-//            every { inputReader.readInput("Select an option: ") } returns "0"
-//
-//            // When
-//            cli.show()
-//
-//            // Then
-//            verify {
-//                outputPrinter.printMessage("=== Manage users ===")
-//                outputPrinter.printMessage("1. Create new user")
-//                outputPrinter.printMessage("2. Delete new user")
-//                outputPrinter.printMessage("0. Back")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `should show error message when enter input incorrectly`() {
-//        runTest {
-//            // Given
-//            every { inputReader.readInput("Select an option: ") } returns "z" andThen "0"
-//
-//            // When
-//            cli.show()
-//
-//            // Then
-//            verify {
-//                outputPrinter.printMessage("=== Manage users ===")
-//                outputPrinter.printMessage("1. Create new user")
-//                outputPrinter.printMessage("2. Delete new user")
-//                outputPrinter.printError("Invalid option.")
-//                outputPrinter.printMessage("0. Back")
-//            }
-//        }
-//    }
-//}
+class UserCLITest {
+    private val inputReader = mockk<InputReader>()
+    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
+    private val createUserCLI = mockk<CreateUserCLI>(relaxed = true)
+    private val deleteUserCLI = mockk<DeleteUserCLI>(relaxed = true)
+    private lateinit var cli: UserCLI
+
+    @BeforeEach
+    fun setUp() {
+        cli = UserCLI(inputReader, outputPrinter, createUserCLI, deleteUserCLI)
+    }
+
+    @Test
+    fun `should show menu and create user when enter 1`() {
+        runTest {
+            // Given
+            every { inputReader.readInput(String.selectOption) } returns "1" andThen "0"
+
+            // When
+            cli.show()
+
+            // Then
+            verify {
+                outputPrinter.printMenuItems(
+                    listOf(
+                        String.manageUsersMenu,
+                        String.createNewUser,
+                        String.deleteUser,
+                        String.back
+                    )
+                )
+            }
+            coVerify {
+                createUserCLI.show()
+            }
+        }
+    }
+
+    @Test
+    fun `should show menu and delete user when enter 2`() {
+        runTest {
+            // Given
+            every { inputReader.readInput(String.selectOption) } returns "2" andThen "0"
+
+            // When
+            cli.show()
+
+            // Then
+            verify {
+                outputPrinter.printMenuItems(
+                    listOf(
+                        String.manageUsersMenu,
+                        String.createNewUser,
+                        String.deleteUser,
+                        String.back
+                    )
+                )
+            }
+            coVerify { deleteUserCLI.show() }
+        }
+    }
+
+    @Test
+    fun `should back when enter 0`() {
+        runTest {
+            // Given
+            every { inputReader.readInput(String.selectOption) } returns "0"
+
+            // When
+            cli.show()
+
+            // Then
+            verify {
+                outputPrinter.printMenuItems(
+                    listOf(
+                        String.manageUsersMenu,
+                        String.createNewUser,
+                        String.deleteUser,
+                        String.back
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `should show error message when enter input incorrectly`() {
+        runTest {
+            // Given
+            every { inputReader.readInput(String.selectOption) } returns "z" andThen "0"
+
+            // When
+            cli.show()
+
+            // Then
+            verify {
+                outputPrinter.printMenuItems(
+                    listOf(
+                        String.manageUsersMenu,
+                        String.createNewUser,
+                        String.deleteUser,
+                        String.back
+                    )
+                )
+                outputPrinter.printError(String.invalidOption)
+            }
+        }
+    }
+}
