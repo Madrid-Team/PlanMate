ca0d2a8fe9327e5b9591bae206c2d64bf4c542a5
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-13 15:30:04 +0300

    refactor create project use case tests

--- src/test/kotlin/domain/usecases/task/CreateTaskUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/task/CreateTaskUseCaseTest.kt
@@ -19,17 +19,14 @@ class CreateTaskUseCaseTest {
     private lateinit var taskRepository: TaskRepository
     private lateinit var taskValidator: TaskValidator
     private lateinit var createTaskUseCase: CreateTaskUseCase
-    private lateinit var getProjectByIdUseCase: GetProjectByIdUseCase
     private lateinit var testScope: TestScope
 
     @BeforeEach
     fun setUp() {
         taskRepository = mockk(relaxed = true)
         taskValidator = mockk(relaxed = true)
-        getProjectByIdUseCase = mockk(relaxed = true)
         createTaskUseCase = CreateTaskUseCase(
             taskRepository,
-            getProjectByIdUseCase,
             taskValidator
         )
         testScope = TestScope()
@@ -44,50 +41,12 @@ class CreateTaskUseCaseTest {
             assertDoesNotThrow {
                 createTaskUseCase.createTask(task)
             }
-            coVerify { getProjectByIdUseCase.getById(task.projectId) }
             coVerify { taskValidator.validateAll(task) }
             coVerify { taskRepository.createTask(task) }
         }
     }
 
-    @Test
-    fun `should throw ProjectNotFoundException when project ID is empty`() {
-        testScope.runTest {
-            // Given
-            val task = createTask(projectId = "")
-            coEvery { getProjectByIdUseCase.getById(task.projectId) } throws
-                    ProjectExceptions.ProjectNotFoundException(
-                        "Project ID cannot be empty"
-                    )
 
-            // When & Then
-            assertThrows<ProjectExceptions.ProjectNotFoundException> {
-                createTaskUseCase.createTask(task)
-            }
-
-            coVerify(exactly = 0) { taskRepository.createTask(any()) }
-        }
-    }
-
-    @Test
-    fun `should throw ProjectNotFoundException when project cannot be found by ID`() {
-        testScope.runTest {
-            // Given
-            val task = createTask(projectId = "invalid-project-id")
-            coEvery { getProjectByIdUseCase.getById(task.projectId) } throws
-                    ProjectExceptions.ProjectNotFoundException(
-                        "Project not found"
-                    )
-
-            // When & Then
-            assertThrows<ProjectExceptions.ProjectNotFoundException> {
-                createTaskUseCase.createTask(task)
-            }
-            coVerify(exactly = 0) { taskValidator.validateAll(any()) }
-            coVerify(exactly = 0) { taskRepository.createTask(any()) }
-        }
-
-    }
 
     @Test
     fun `should throw TaskTitleIsEmptyException when task title is empty`() {
