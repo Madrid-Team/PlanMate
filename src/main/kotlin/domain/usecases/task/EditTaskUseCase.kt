b7e9475614d6a8a0d83c8ccf829c007c1e15ef71
khater <khater@email.com>
2025-05-13 14:10:45 +0300

    remove unused validator for project

--- src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/task/EditTaskUseCase.kt
@@ -7,15 +7,9 @@ import domain.usecases.project.GetProjectByIdUseCase
 class EditTaskUseCase(
     private val taskRepository: TaskRepository,
     private val getProjectByIdUseCase: GetProjectByIdUseCase,
-    private val getTaskByIdUseCase: GetTaskByIdUseCase,
     private val taskValidator: TaskValidator
 ) {
     suspend fun editTask(task: Task) {
-        // Verify project exists
-        getProjectByIdUseCase.getById(task.projectId)
-
-        // Verify task exists
-        getTaskByIdUseCase.getTaskById(task.projectId, task.id.toString())
 
         taskValidator.validateBasic(task)
         taskRepository.editTask(task)
