cb7d8460e871539d8a4298227f00346adf95dc16
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-13 18:35:48 +0300

    refactor ValidateAdminRoleUseCase

--- src/main/kotlin/domain/usecases/user/ValidateAdminRoleUseCase.kt
+++ src/main/kotlin/domain/usecases/user/ValidateAdminRoleUseCase.kt
@@ -3,10 +3,14 @@ package domain.usecases.user
 import domain.models.authentication.UserRole
 
 import domain.utils.AdminValidationResult
+import domain.utils.UserExceptions
 
 
 class ValidateAdminRoleUseCase() {
      fun validate(role: String): Boolean {
+         if (role != "ADMIN"){
+           throw UserExceptions.UserNotAdminException()
+         }
          return role == UserRole.ADMIN.name
     }
 }
