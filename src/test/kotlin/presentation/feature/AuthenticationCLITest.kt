95727e957e8a7fd2ffd579c38f3ced489197b64a
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-13 19:09:02 +0300

    fix tests

--- src/test/kotlin/presentation/feature/AuthenticationCLITest.kt
+++ src/test/kotlin/presentation/feature/AuthenticationCLITest.kt
@@ -1,6 +1,6 @@
 package presentation.feature
 
-import data.utils.PasswordHasher
+import com.google.common.truth.Truth.assertThat
 import domain.models.authentication.User
 import domain.usecases.user.LoginUserUseCase
 import io.mockk.coEvery
@@ -11,122 +11,78 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import presentation.components.InputReader
 import presentation.components.OutputPrinter
+import presentation.utils.*
 import java.util.*
 import kotlin.test.Test
 
-//class AuthenticationCLITest {
-//    private val inputReader = mockk<InputReader>()
-//    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
-//    private val useCase = mockk<LoginUserUseCase>()
-//    private lateinit var cli: AuthenticationCLI
-//
-//    @BeforeEach
-//    fun setUp() {
-//        cli = AuthenticationCLI(inputReader, outputPrinter, useCase)
-//    }
-//
-//    @Test
-//    fun `should login success when user name and password is correct`() {
-//        runTest {
-//            // Given
-//            val username = "user name"
-//            val password = "password"
-//            val passwordHash = PasswordHasher.hash(password)
-//            val user = User(id = UUID.randomUUID(), username, passwordHash, "MATE")
-//
-//            every { inputReader.readInput() } returnsMany listOf(username, password)
-//            coEvery { useCase.login(username, passwordHash) } returns user
-//
-//            // When
-//            cli.login()
-//
-//            // Then
-//            verify {
-//                outputPrinter.printMessage("=== Login ===")
-//                outputPrinter.printMessage("Enter user name:")
-//                outputPrinter.printMessage("Enter password:")
-//                outputPrinter.printMessage("Login Success")
-//            }
-//            verify(exactly = 0) { outputPrinter.printMessage("Login error:(") }
-//        }
-//    }
-//
-//    @Test
-//    fun `should handle exception and show Login error message when user name and password is incorrect`() {
-//        runTest {
-//            // Given
-//            val username = "user name"
-//            val password = "password"
-//            val passwordHash = PasswordHasher.hash(password)
-//
-//            every { inputReader.readInput() } returnsMany listOf(username, password, "z") // 2 = don't retry
-//            coEvery { useCase.login(username, passwordHash) } throws Exception()
-//
-//            // When
-//            cli.login()
-//
-//            // Then
-//            verify {
-//                outputPrinter.printMessage("Login error:(")
-//                outputPrinter.printMessage("if you want to try again enter \"1\" else enter anything")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `should retry login when exception occurs and user enters 1`() {
-//        runTest {
-//            // Given
-//            val correctUsername = "user name 1"
-//            val correctPassword = "password 1"
-//            val incorrectUsername = "user name 2"
-//            val incorrectPassword = "password 2"
-//            val correctHash = PasswordHasher.hash(correctPassword)
-//            val incorrectHash = PasswordHasher.hash(incorrectPassword)
-//            val user = User(id = UUID.randomUUID(), correctUsername, correctHash, "MATE")
-//
-//            every { inputReader.readInput() } returnsMany listOf(
-//                incorrectUsername,
-//                incorrectPassword,
-//                "1",
-//                correctUsername,
-//                correctPassword
-//            )
-//            coEvery { useCase.login(incorrectUsername, incorrectHash) } throws Exception()
-//            coEvery { useCase.login(correctUsername, correctHash) } returns user
-//
-//            // When
-//            cli.login()
-//
-//            // Then
-//            verify(exactly = 2) {
-//                outputPrinter.printMessage("Enter user name:")
-//                outputPrinter.printMessage("Enter password:")
-//            }
-//            verify(exactly = 1) { outputPrinter.printMessage("Login error:(") }
-//        }
-//    }
-//
-//    @Test
-//    fun `should not retry login when exception occurs and user don't enters 1`() {
-//        runTest {
-//            // Given
-//            val incorrectUsername = "user name 2"
-//            val incorrectPassword = "password 2"
-//            val incorrectHash = PasswordHasher.hash(incorrectPassword)
-//
-//            every { inputReader.readInput() } returnsMany listOf(incorrectUsername, incorrectPassword, "z")
-//            coEvery { useCase.login(incorrectUsername, incorrectHash) } throws Exception()
-//
-//            // When
-//            cli.login()
-//
-//            // Then
-//            verify(exactly = 1) {
-//                outputPrinter.printMessage("Enter user name:")
-//                outputPrinter.printMessage("Enter password:")
-//                outputPrinter.printMessage("Login error:(")
-//            }
-//        }
-//    }
-//}
+class AuthenticationCLITest {
+    private val inputReader = mockk<InputReader>()
+    private val outputPrinter = mockk<OutputPrinter>(relaxed = true)
+    private val useCase = mockk<LoginUserUseCase>()
+    private lateinit var cli: AuthenticationCLI
+
+    @BeforeEach
+    fun setUp() {
+        cli = AuthenticationCLI(inputReader, outputPrinter, useCase)
+    }
+
+    @Test
+    fun `should login success when user name and password is correct`() {
+        runTest {
+            // Given
+            val username = "user name"
+            val password = "password"
+            val user = User(id = UUID.randomUUID(), username = username, passwordHash = password, "MATE")
+            every { inputReader.readInput() } returnsMany listOf(username, password)
+            coEvery { useCase.login(username, password) } returns user
+
+            // When
+            cli.login()
+
+            // Then
+            verify {
+                outputPrinter.printMessage(String.loginHeader)
+                outputPrinter.printMessage(String.enterUserName)
+                outputPrinter.printMessage(String.enterPassword)
+                outputPrinter.printMessage(String.loginSuccess)
+            }
+            verify(exactly = 0) { outputPrinter.printMessage("Login error:(") }
+        }
+    }
+
+    @Test
+    fun `should handle exception and show Login error message when user name and password is incorrect`() {
+        runTest {
+            // Given
+            val username = "user name"
+            val password = "password"
+            val invalidUsername = "invalid user name"
+            val invalidPassword = "invalid password"
+            val errorMessage = "Username or password is incorrect"
+            val user = User(id = UUID.randomUUID(), username = username, passwordHash = password, "MATE")
+
+            every { inputReader.readInput() } returnsMany listOf(invalidUsername, invalidPassword, username, password)
+            coEvery { useCase.login(invalidUsername, invalidPassword) } throws Exception(errorMessage)
+            coEvery { useCase.login(username, password) } returns user
+
+            // When
+            val result = cli.login()
+
+            // Then
+            verify {
+                outputPrinter.printMenuItems(
+                    listOf(
+                        String.loginError,
+                        errorMessage,
+                        String.pleaseTryAgain
+                    )
+                )
+                outputPrinter.printMessage(String.loginHeader)
+                outputPrinter.printMessage(String.enterUserName)
+                outputPrinter.printMessage(String.enterPassword)
+                outputPrinter.printMessage(String.loginSuccess)
+            }
+            assertThat(result).isEqualTo(user)
+        }
+    }
+}
