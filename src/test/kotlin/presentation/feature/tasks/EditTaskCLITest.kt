48ab3910e48d1821f5142306fbe96eb22b052009
IslamMagd <eslam752001.magdy54@gmail.com>
2025-05-13 16:35:55 +0300

    test: update test cases to reflect recent CurrentUserProvider in EditTaskCLITest.kt

--- src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
+++ src/test/kotlin/presentation/feature/tasks/EditTaskCLITest.kt
@@ -1,10 +1,11 @@
 package presentation.feature.tasks
 
+import data.mapper.toDto
+import data.source.user.CurrentUserProvider
 import domain.models.authentication.User
 import domain.models.authentication.UserRole
 import domain.models.logs.CurrentUser
 import domain.usecases.task.EditTaskUseCase
-import domain.utils.TaskExceptions
 import io.mockk.*
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
@@ -14,67 +15,51 @@ import presentation.components.OutputPrinter
 import java.util.*
 import kotlin.test.Test
 
-//class EditTaskCLITest {
-//    private lateinit var inputReader: InputReader
-//    private lateinit var outputPrinter: OutputPrinter
-//    private lateinit var editTaskUseCase: EditTaskUseCase
-//    private lateinit var taskView: TaskView
-//    private lateinit var editTaskCLI: EditTaskCLI
-//    private lateinit var testScope: TestScope
-//
-//    @BeforeEach
-//    fun setUp() {
-//        inputReader = mockk(relaxed = true)
-//        outputPrinter = mockk(relaxed = true)
-//        editTaskUseCase = mockk(relaxed = true)
-//        taskView = mockk(relaxed = true)
-//        editTaskCLI = EditTaskCLI(inputReader, outputPrinter, editTaskUseCase)
-//        testScope = TestScope()
-//
-//        val testUser = User(id = UUID.randomUUID(), "hhdhdh", "sgsgsggs", UserRole.ADMIN.name)
-//        mockkObject(CurrentUser)
-//        every { CurrentUser.getCurrentUser() } returns testUser
-//    }
-//
-//
-//    @Test
-//    fun `should edit task successfully when call edit task`() {
-//        testScope.runTest {
-//            every { inputReader.readInput(any()) } returnsMany listOf(
-//                UUID.randomUUID().toString(),
-//                UUID.randomUUID().toString(),
-//                "New Title",
-//                "New Description"
-//            )
-//            val updatedTask = helperTask(
-//                projectId = UUID.randomUUID().toString(),
-//                id = UUID.randomUUID().toString(),
-//                title = "New Title",
-//                description = "New Description"
-//            )
-//            coEvery { editTaskUseCase(updatedTask) } returns Unit
-//
-//            editTaskCLI.show()
-//
-//            verify { outputPrinter.printMessage("Task updated successfully") }
-//        }
-//    }
-//
-//    @Test
-//    fun `should show error message when task update fails`() {
-//        testScope.runTest {
-//            every { inputReader.readInput(any()) } returnsMany listOf(
-//                UUID.randomUUID().toString(),
-//                UUID.randomUUID().toString(),
-//                "New Title",
-//                "New Description"
-//            )
-//
-//            coEvery { editTaskUseCase(any()) } throws TaskExceptions.TaskCannotEditException()
-//
-//            editTaskCLI.show()
-//
-//            verify { outputPrinter.printError(TaskExceptions.TaskCannotEditException().message!!) }
-//        }
-//    }
-//}
+class EditTaskCLITest {
+    private lateinit var inputReader: InputReader
+    private lateinit var outputPrinter: OutputPrinter
+    private lateinit var editTaskUseCase: EditTaskUseCase
+    private lateinit var currentUserProvider: CurrentUserProvider
+    private lateinit var taskViewer: TaskViewer
+    private lateinit var editTaskCLI: EditTaskCLI
+    private lateinit var testScope: TestScope
+
+    @BeforeEach
+    fun setUp() {
+        inputReader = mockk(relaxed = true)
+        outputPrinter = mockk(relaxed = true)
+        editTaskUseCase = mockk(relaxed = true)
+        currentUserProvider = mockk()
+        taskViewer = mockk(relaxed = true)
+        editTaskCLI = EditTaskCLI(inputReader, outputPrinter, editTaskUseCase, currentUserProvider)
+        testScope = TestScope()
+
+        val testUser = User(id = UUID.randomUUID(), "hhdhdh", "sgsgsggs", UserRole.ADMIN.name)
+        mockkObject(CurrentUser)
+        every { currentUserProvider.getCurrentUser() } returns testUser.toDto()
+    }
+
+
+    @Test
+    fun `should edit task successfully when call edit task`() {
+        runTest {
+            every { inputReader.readInput(any()) } returnsMany listOf(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                "New Title",
+                "New Description"
+            )
+            val updatedTask = helperTask(
+                projectId = UUID.randomUUID().toString(),
+                id = UUID.randomUUID().toString(),
+                title = "New Title",
+                description = "New Description"
+            )
+            coEvery { editTaskUseCase.editTask(updatedTask) } returns Unit
+
+            editTaskCLI.show()
+
+            verify { outputPrinter.printMessage("Task updated successfully") }
+        }
+    }
+}
