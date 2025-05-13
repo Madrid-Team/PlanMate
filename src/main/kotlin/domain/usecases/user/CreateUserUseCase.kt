7924b8947eb6dcae7f11c5275c7f8ad7ae82e306
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-13 10:37:58 +0300

    update

--- src/main/kotlin/domain/usecases/user/CreateUserUseCase.kt
+++ src/main/kotlin/domain/usecases/user/CreateUserUseCase.kt
@@ -1,26 +1,35 @@
 package domain.usecases.user
 
 import domain.models.authentication.User
+import domain.models.authentication.UserRole
 import domain.repository.UserRepository
-import domain.usecases.user.ValidateUser
 import domain.utils.NameValidationResult
 import domain.utils.PasswordValidationResult
 import domain.utils.UserExceptions
+import java.util.*
 
 class CreateUserUseCase(
     private val userRepository: UserRepository,
     private val validateNameUseCase: ValidateNameUseCase,
-    private val validatePasswordUseCase: ValidatePasswordUseCase
+    private val validatePasswordUseCase: ValidatePasswordUseCase,
+    private val passwordHashUseCase: PasswordHashUseCase,
 ) {
-    suspend fun createUser(user: User) {
-        val nameValidation = validateNameUseCase.validateName(user.username)
+    suspend fun createUser(userName: String, password: String) {
+        val nameValidation = validateNameUseCase.validateName(userName)
         if (nameValidation is NameValidationResult.NotValid) {
             throw UserExceptions.InvalidUserName()
         }
-        val passwordValidation = validatePasswordUseCase.validatePassword(user.passwordHash)
+        val passwordValidation = validatePasswordUseCase.validatePassword(password)
         if (passwordValidation is PasswordValidationResult.NotValid) {
             throw UserExceptions.InvalidPasswordError()
         }
+        val passwordHash = passwordHashUseCase.passwordHash(password)
+        val user = User(
+            id = UUID.randomUUID(),
+            username = userName,
+            passwordHash = passwordHash,
+            role = UserRole.MATE.name,
+        )
         userRepository.createNewUser(user)
-}
     }
+}
