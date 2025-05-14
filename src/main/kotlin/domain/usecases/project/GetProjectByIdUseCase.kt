8cca3c2d9ed3073cfaaa2477f1668b9cf4da2069
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-14 16:30:53 +0300

    edit function name from execute to getProjectLogsByProjectId

--- src/main/kotlin/domain/usecases/project/GetProjectByIdUseCase.kt
+++ src/main/kotlin/domain/usecases/project/GetProjectByIdUseCase.kt
@@ -7,10 +7,10 @@ import domain.utils.ProjectExceptions
 class GetProjectByIdUseCase(
     private val projectRepository: ProjectRepository
 ) {
-    suspend fun getProjectById(id: String): Project {
-        if (id.isBlank()) {
+    suspend fun getProjectById(projectId: String): Project {
+        if (projectId.isBlank()) {
             throw ProjectExceptions.ProjectNotFoundException("Project ID cannot be empty")
         }
-        return projectRepository.getProjectById(id)
+        return projectRepository.getProjectById(projectId)
     }
 }
