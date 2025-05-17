3a22ac2911a87506c9a11d862e300feacb52a28a
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-05-17 15:18:58 +0300

    update use cases koin module

--- src/main/kotlin/di/useCasesModule.kt
+++ src/main/kotlin/di/useCasesModule.kt
@@ -13,24 +13,25 @@ val useCasesModule = module {
     single { ValidateAdminRoleUseCase() }
     single { CreateProjectUseCase(get(), get()) }
     single { DeleteProjectUseCase(get(), get()) }
-    single { EditProjectUseCase(get(), get(), get()) }
+    single { EditProjectUseCase(get(), get(), get(), get(), get()) }
     single { CreateProjectUseCase(get(), get()) }
     single { CreateProjectUseCase(get(), get()) }
     single { DeleteProjectUseCase(get(), get()) }
-    single { EditProjectUseCase(get(), get(), get()) }
+    single { EditProjectUseCase(get(), get(), get(), get(), get()) }
     single { GetAllProjectsUseCase(get()) }
     single { GetProjectLogsByIdUseCase(get(), get()) }
     single { GetProjectByIdUseCase(get()) }
-    single { CreateUserUseCase(get(), get(), get(),get()) }
+    single { CreateUserUseCase(get(), get(), get(), get()) }
     single { DeleteUserUseCase(get(), get()) }
     single { LoginUserUseCase(get(), get(), get()) }
     single { DeleteTaskUseCase(get()) }
     single { CreateTaskUseCase(get(), get()) }
-    single { EditTaskUseCase(get(), get()) }
+    single { EditTaskUseCase(get(), get(), get(), get(), get()) }
     single { GetTaskLogsUseCase(get()) }
     single { GetTasksByProjectIdUseCase(get()) }
     single { TaskValidator() }
     single { ProjectValidator() }
     single { AddAuditLogUseCase(get()) }
+    single { GetTaskByIdUseCase(get()) }
 
 }
